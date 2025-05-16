0331247a91b80c8438580bf485d96dd453f39d87
Andrii Soroka <vt231_sao@student.ztu.edu.ua>
2025-05-16 18:08:48 +0300

    замість глобальної змінної  додано функцію getDbConnection().

--- db_config.php
+++ db_config.php
@@ -1,11 +1,14 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$dbname = 'minesweeper';
+function getDbConnection(): mysqli {
+    $host = 'localhost';
+    $user = 'root';
+    $password = '';
+    $dbname = 'minesweeper';
 
-$conn = new mysqli($host, $user, $password, $dbname);
-if ($conn->connect_error) {
-    die("Помилка підключення: " . $conn->connect_error);
+    $conn = new mysqli($host, $user, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Помилка підключення: " . $conn->connect_error);
+    }
+    return $conn;
 }
 ?>
