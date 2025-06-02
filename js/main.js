5145bb7a6cc930fe5ca29b80673ec18e1516c2f7
Shadocl-low <vt231_poi@student.ztu.edu.ua>
2025-06-02 14:17:10 +0300

    Replaced magick numbers with constants

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 import { GameTimer } from './timer.js';
 import { Board } from './board.js';
+import { EasyDifficulty, MediumDifficulty, HardDifficulty } from './difficulty-strategies.js';
 
 const gameContainer = document.getElementById("game");
 const timerDisplay = document.getElementById("timer");
 const timer = new GameTimer(timerDisplay);
-let game = new Board(9, 9, 10, gameContainer, timer, 'easy');
+let game = new Board(new EasyDifficulty(), gameContainer, timer, 'easy');
 
 document.querySelectorAll('#difficulty button').forEach(button => {
     button.addEventListener('click', () => {
